d162974e3abff591be5e3b118f9d4a2d3b28b5cb
Reetik Rajan <r4reetik@gmail.com>
2022-01-27 12:44:39 +0530

    class for our blockchain
    + constructor

--- 01-openquest-funofblockchain.py
+++ 01-openquest-funofblockchain.py
@@ -4,3 +4,10 @@ import hashlib
 from hashlib import sha256
 import json
 import time
+
+
+class Chain:
+    def __init__(self) -> None:
+        self.blockchain = []
+        self.pending = []
+        self.add_block(prevhash="Genesis", proof=1708)
