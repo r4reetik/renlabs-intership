f52988a3be24d461943cf550590858a7d77bb8b8
Reetik Rajan <r4reetik@gmail.com>
2022-01-27 12:59:27 +0530

    required methods
    + Add transaction
    + Compute Hash
    + Add Block

--- 01-openquest-funofblockchain.py
+++ 01-openquest-funofblockchain.py
@@ -1,6 +1,3 @@
-import hashlib
-
-
 from hashlib import sha256
 import json
 import time
@@ -11,3 +8,30 @@ class Chain:
         self.blockchain = []
         self.pending = []
         self.add_block(prevhash="Genesis", proof=1708)
+
+    def add_transaction(self, sender, recipient, amount):
+        transaction = {
+            "Sender": sender,
+            "recipient": recipient,
+            "amount": amount
+        }
+
+        self.pending.append(transaction)
+
+    def compute_hash(self, block):
+        json_block = json.dumps(block, sort_keys=True).encode()
+        block_hash = sha256(json_block).hexdigest()
+
+        return block_hash
+
+    def add_block(self, proof, prevhash=None):
+        block = {
+            "index": len(self.blockchain),
+            "timestamp": time.time(),
+            "transactions": self.pending,
+            "proof": proof,
+            "prevhash": prevhash
+        }
+
+        self.pending = []
+        self.blockchain.append(block)
